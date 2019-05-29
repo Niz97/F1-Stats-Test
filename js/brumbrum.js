a04a34d1f63b86dd899e468603bc394d8f573e5f
Niz97 <97niran@gmail.com>
2019-05-29 20:57:00 +0100

    Implemented callbacks
    Code now makes use of callbacks.
    Previously I think the problem was that the pie chart was trying to be drawn before the API request and array had been populated.

--- js/brumbrum.js
+++ js/brumbrum.js
@@ -1,61 +1,44 @@
+function load_standings(callback) {
+  const http_req = new XMLHttpRequest();
+  const url = 'https://ergast.com/api/f1/current/constructorStandings.json';
+  http_req.open('GET', url);
+  http_req.send();
 
-google.charts.load('current', {'packages':['corechart']});
+  http_req.onreadystatechange = function(e) {
+    if (http_req.readyState === 4 && http_req.status === 200) {
+      let response_data = JSON.parse(http_req.responseText);
 
-var constructorStandings = [];
-getData();
+      let standings = response_data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
 
-google.charts.setOnLoadCallback(drawChart);
-
-
-
-function drawChart() {
-
-var data = google.visualization.arrayToDataTable(constructorStandings);
-
-var options = {
-  title: 'F1 2019'
-};
-
-var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-chart.draw(data, options);
-}
-
-
-
-function getData() {
-let data = null;
-
-const http_req = new XMLHttpRequest();
-const url = 'https://ergast.com/api/f1/current/constructorStandings.json';
-http_req.open("GET", url);
-http_req.send();
-
-http_req.onreadystatechange = function(e) {
-
-  if(http_req.readyState === 4 && http_req.status === 200) {
-
-    data = JSON.parse(http_req.responseText);
-
-    let standings = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-
-    for (let i = 0; i < standings.length; i++) {
+      for (let i = 0; i < standings.length; i++) {
         let standing = standings[i];
-        console.log('#' + standing.position, standing.Constructor.name, "-", standing.points);
-    }
-
-    // add constructor names and respective points to constructorStandings array
-    for (let i = 0; i < standings.length; i++) {
-        let standing = standings[i];
-        constructorStandings.push([standing.Constructor.name, parseInt(standing.points, 10)]);
-    }
-
-    // pie chart categories
-    constructorStandings.unshift(['Team', 'Points']);
-
-
-  }
+        console.log("#" + standing.position, standing.Constructor.name,"-", standing.points);
+      }
       
+      // Table "columns"
+      var standings_table = [['Team', 'Points']];
+
+      // add constructor names and respective points to constructorStandings array
+      for (let i = 0; i < standings.length; i++) {
+        let standing = standings[i];
+        standings_table.push([standing.Constructor.name, parseInt(standing.points, 10)]);
+      }
+
+      callback(standings_table);
+    }
+  };
 }
+
+
+window.onload = function() {
+    load_standings(function(standings_table) {
+      google.charts.load('current', { packages: ['corechart'] });
+      google.charts.setOnLoadCallback(function() {
+        var chart_standings_table = google.visualization.arrayToDataTable(standings_table);
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        var options = { title: 'F1 2019' };
+        chart.draw(chart_standings_table, options);
+      });
+    });
 
 }
