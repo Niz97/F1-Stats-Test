b76c5d657880daff46db09d33469b51c73045988
Niz97 <97niran@gmail.com>
2019-05-29 22:10:31 +0100

    Implemented top 5 drivers
    Added pie chart to show top 5 drivers standings

--- js/brumbrum.js
+++ js/brumbrum.js
@@ -1,4 +1,4 @@
-function load_standings(callback) {
+function load_constructor_standings(callback) {
   const http_req = new XMLHttpRequest();
   const url = 'https://ergast.com/api/f1/current/constructorStandings.json';
   http_req.open('GET', url);
@@ -12,7 +12,7 @@ function load_standings(callback) {
 
       for (let i = 0; i < standings.length; i++) {
         let standing = standings[i];
-        console.log("#" + standing.position, standing.Constructor.name,"-", standing.points);
+        // console.log("#" + standing.position, standing.Constructor.name,"-", standing.points);
       }
       
       // Table "columns"
@@ -29,16 +29,57 @@ function load_standings(callback) {
   };
 }
 
+function load_driver_standings(callback) {
+    const http_req = new XMLHttpRequest();
+    const url = 'https://ergast.com/api/f1/current/driverStandings.json';
+    http_req.open('GET', url);
+    http_req.send();
+
+    http_req.onreadystatechange = function(e) {
+        if (http_req.readyState === 4 && http_req.status === 200) {
+        let response_data = JSON.parse(http_req.responseText);
+
+        let standings = response_data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+        //console.log(standings);
+
+        var driver_standings = [['Driver', 'Points']];
+
+        for (var i = 0; i < 5; i++) {
+            driver_standings.push([standings[i].Driver.driverId, parseInt(standings[i].points,10)]);
+            // console.log(standings[i].Driver.driverId + ": " + standings[i].points);
+        }
+
+        // console.log(driver_standings);
+
+
+        callback(driver_standings);
+
+    
+        }
+    }
+}
+
 
 window.onload = function() {
-    load_standings(function(standings_table) {
-      google.charts.load('current', { packages: ['corechart'] });
-      google.charts.setOnLoadCallback(function() {
-        var chart_standings_table = google.visualization.arrayToDataTable(standings_table);
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-        var options = { title: 'F1 2019' };
-        chart.draw(chart_standings_table, options);
+    // constructors championship
+    load_constructor_standings(function(standings_table) {
+        google.charts.load('current', { packages: ['corechart'] });
+        google.charts.setOnLoadCallback(function() {
+            var chart_standings_table = google.visualization.arrayToDataTable(standings_table);
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            var options = { title: 'F1 2019' };
+            chart.draw(chart_standings_table, options);
       });
     });
 
+    // top 5 in driver standings
+    load_driver_standings(function(driver_standings) {
+        google.charts.load('current', { packages: ['corechart'] });
+        google.charts.setOnLoadCallback(function() {
+            var top_five = google.visualization.arrayToDataTable(driver_standings);
+            var chart = new google.visualization.PieChart(document.getElementById('TopFivepiechart'));
+            var options = { title: 'Top 5 Drivers' };
+            chart.draw(top_five, options);
+      });
+    });
 }
